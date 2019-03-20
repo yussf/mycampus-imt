cfb2fac337ed6da93bcf94a9b2ed955811538fa0
Youssef <dunnowuut@gmail.com>
2019-03-20 21:28:25 +0100

    Test1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const  isResident = require('./utils.js');
+console.log("ttttttttttttttttttttttt");
 // Imports dependencies and set up http server
 const
   request = require('request'),
