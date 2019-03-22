9771c336d122ade4f4bc65ea85fa5cc9e2cea749
Youssef <dunnowuut@gmail.com>
2019-03-22 15:45:42 +0100

    HandlePostBack deleted.

--- app.js
+++ app.js
@@ -114,21 +114,6 @@ request(curl, function (error, response, body) {
 
 }
 
-function handlePostback(sender_psid, received_postback) {
-  console.log('ok')
-   let response;
-  // Get the payload for the postback
-  let payload = received_postback.payload;
-
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, try sending another image." }
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
-}
 
 function callSendAPI(sender_psid, response) {
   // Construct the message body
