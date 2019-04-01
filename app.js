13f3cc0f8a8f67279ae45b00aa6ebf79ccf6e3cc
Youssef <dunnowuut@gmail.com>
2019-04-01 22:30:45 +0200

    require path corrected

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const utils = require('./utils.js');
-const facebook = require('libs/facebook.js');
+const facebook = require('/libs/facebook.js');
 // Imports dependencies and set up http server
 const
   request = require('request'),
