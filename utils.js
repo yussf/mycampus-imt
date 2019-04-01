65f0a1dd390cacad60b7537e24a93de368a3430b
Youssef <dunnowuut@gmail.com>
2019-04-02 00:04:16 +0200

    db_coonector seperated

--- utils.js
+++ utils.js
@@ -3,22 +3,21 @@ const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
-
+const db = require('./db_connector.js') ;
 module.exports = {
    isResident:function(fname,lname,fb_id) {
        return new Promise(function(resolve) {
-             client.connect();
-             client.query('SELECT fb_id, fname, lname FROM users', (err, res) => {
-               if (err) throw err;
-               for (let row of res.rows) {
-                 let o = JSON.parse(JSON.stringify(row)) ;
-                 if (o.fname==fname & o.lname==lname){
-                   //console.log("plopm");
-                   resolve(true) ;
-                 }
-               }
-               client.end();
-             });
+         db.query('SELECT fb_id, fname, lname FROM users',(err, res) => {
+                   if (err) throw err;
+                   for (let row of res.rows) {
+                     let o = JSON.parse(JSON.stringify(row)) ;
+                     if (o.fname==fname & o.lname==lname){
+                       //console.log("plopm");
+                       resolve(true) ;
+                     }
+                   }
+
+                 });
            });
         }
 };
