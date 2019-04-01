80554a7b37338ff066bf7f2bc90f8f2b688e5924
Youssef <dunnowuut@gmail.com>
2019-04-01 21:59:24 +0200

    Multiple exports in utils.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const  isResident = require('./utils.js');
+const  utils = require('./utils.js');
 // Imports dependencies and set up http server
 const
   request = require('request'),
@@ -11,7 +11,7 @@ const
 ////////////////////////////////////////////////////
 //comment
 ///////////////////////////////////////////////////
-let ir = isResident("Youssef", "DOUBLI", "5895654698") ;
+let ir = utils.isResident("Youssef", "DOUBLI", "5895654698") ;
 ir.then((data) => console.log(data)) ;
 /////////////////////////////////////////////////
 // Sets server port and logs message on success
--- utils.js
+++ utils.js
@@ -3,20 +3,22 @@ const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
-function isResident(fname,lname,fb_id) {
-    return new Promise(function(resolve) {
-          client.connect();
-          client.query('SELECT fb_id, fname, lname FROM users', (err, res) => {
-            if (err) throw err;
-            for (let row of res.rows) {
-              let o = JSON.parse(JSON.stringify(row)) ;
-              if (o.fname==fname & o.lname==lname){
-                //console.log("plopm");
-                resolve(true) ;
-              }
-            }
-            client.end();
-          });
-        });
-     }
-module.exports = isResident;
+
+module.exports = {
+   isResident:function(fname,lname,fb_id) {
+       return new Promise(function(resolve) {
+             client.connect();
+             client.query('SELECT fb_id, fname, lname FROM users', (err, res) => {
+               if (err) throw err;
+               for (let row of res.rows) {
+                 let o = JSON.parse(JSON.stringify(row)) ;
+                 if (o.fname==fname & o.lname==lname){
+                   //console.log("plopm");
+                   resolve(true) ;
+                 }
+               }
+               client.end();
+             });
+           });
+        };
+};
