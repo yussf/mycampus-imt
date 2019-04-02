5107431f6868bb4e5feba01edffab88a635c2e1f
Youssef <dunnowuut@gmail.com>
2019-04-02 20:35:03 +0200

    debugging : no -m

--- libs/facebook.js
+++ libs/facebook.js
@@ -24,8 +24,8 @@ module.exports = {
                         }
                         // Send the response message
                         module.exports.callSendAPI(sender_psid, response);
-                      }*/
-                  }) ;
+                      }
+                  }) ;*/
           });
               // Create the payload for a basic text message, which
               // will be added to the body of our request to the Send API
