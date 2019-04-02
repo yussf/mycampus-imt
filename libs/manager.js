f48c487a61765c7ec3bd1d7af43855050b28ca51
Youssef <dunnowuut@gmail.com>
2019-04-03 01:06:26 +0200

    newUser(fb_id) added to manager.js

--- libs/manager.js
+++ libs/manager.js
@@ -1,4 +1,5 @@
 const { Client } = require('pg');
+const uuidv1 = require('uuid/v1');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
@@ -8,7 +9,7 @@ module.exports = {
   isResident:function(fname,lname,fb_id) {
       return new Promise(function(resolve) {
             client.query('SELECT fb_id, first_name, last_name FROM users', (err, res) => {
-
+              if (err) throw err;
               let echo = false ;
               if (res != undefined){
                 console.log(res.rows);
@@ -23,5 +24,16 @@ module.exports = {
               resolve(echo);
             });
           });
-       }
+       },
+  newUser:function(fb_id){
+    let uuid = uuidv1() ;
+    return new Promise(function(resolve) {
+          client.query("INSERT INTO verification (fb_id, guid,timestamp)"+
+                "VALUES ('"+fb_id+"','"+uuid+"','"+Date.now()+"')", (err, res) => {
+                  if (err) throw err;
+                  resolve(echo);
+          });
+        });
+
+  }
 };
