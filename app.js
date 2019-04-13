508d81ff54abc6d8b156da8f7791f154546729db
Youssef <dunnowuut@gmail.com>
2019-04-13 22:56:17 +0200

    add cheerio to packages.json

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 'use strict';
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const manager = require('./libs/manager.js');
-const verifyWebhook = require('./libs/verify-webhook');
-const messageWebhook = require('./libs/message-webhook');
 const messageHandler = require('./libs/message-handler');
+const scrapper = require('./libs/scrapper.js');
 
 const verify = require('./libs/verify_lib.js');
 // Imports dependencies and set up http server
@@ -21,3 +18,4 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 app.post('/dialogflow', messageHandler);
 // Accepts GET requests at the /webhook endpoint
 app.get('/verify/:userId/:uuid', verify);
+app.get('/scrap', scrapper) ;
--- libs/scrapper.js
+++ libs/scrapper.js
@@ -1,9 +1,22 @@
-const { Client } = require('pg');
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+var cheerio = require('cheerio');
+module.exports = (req, res) => {
+  // The URL we will scrape from - in our example Anchorman 2.
 
-module.exports = {
-   
+  url = 'http://services.imt-atlantique.fr/rak/';
+
+  // The structure of our request call
+  // The first parameter is our URL
+  // The callback function takes 3 parameters, an error, response status code and the html
+
+  request(url, function(error, response, html){
+
+      // First we'll check to make sure no errors occurred when making the request
+
+      if(!error){
+          // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+          var $ = cheerio.load(html);
+        //to do 
+      }
+  })
 };
