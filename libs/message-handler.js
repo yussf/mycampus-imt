32455b5db6e1383e9b03d5fda0f3002395a903b2
Youssef <dunnowuut@gmail.com>
2019-04-13 15:42:14 +0200

    debuggin

--- libs/message-handler.js
+++ libs/message-handler.js
@@ -7,35 +7,29 @@ module.exports = (req, res) => {
   let sender_psid = facebook_req.payload.data.sender.id ;
   let msg_text = req.body.queryResult.queryText ;
   let intent = req.body.queryResult.intent.name ;
-  let res_msgs = req.body.queryResult.fulfillmentMessages[1].text.text;
+  let res_msgs = req.body.queryResult.fulfillmentMessages[0].text.text;
   console.log(res_msgs);
   console.log(intent);
+  switch (intent) {
+    case "smalltalk.greetings.hello":
+          manager.isResident(sender_psid);
+          .then((data) => {
+                console.log(data);
+                console.log(sender_psid);
+                if (data == true){
+                  response = {} ;
+                }else{
+                  response = {} ;
+                }
 
-  if ( msg_text == "Hi"){
-      let promise = manager.isResident(sender_psid);
-      promise.then((data) => {
-            console.log(data);
-            console.log(sender_psid);
-            if (data == true){
-              response = {} ;
-            }else{
-              response = {} ;
-            }
+                res.setHeader('Content-Type','applicaiton/json');
+                res.send(response);
 
-            res.setHeader('Content-Type','applicaiton/json');
-            res.send(response);
+            })
+            .catch((err) => console.log(err)) ;
+      break;
+    default:
 
-        }) ;
-      promise.catch((err) => console.log(err)) ;
-
-      }else if (msg_text.indexOf("@imt-atlantique.net") > 1) {
-        manager.newUser(sender_psid,msg_text);
-        response = {
-          "fulfillmentText": "I have sent you an email to "+msg_text+" to verify your account. Check it out."
-        } ;
-        res.setHeader('Content-Type','applicaiton/json');
-        res.send(response);
-      }
-
+  }
 
 };
