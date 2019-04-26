8fc9f8464f0d1b890c8678151a05e17f71ed876c
Youssef <dunnowuut@gmail.com>
2019-04-27 00:08:35 +0200

    message not sent BUG

--- libs/notifier.js
+++ libs/notifier.js
@@ -10,7 +10,7 @@ module.exports = function(){
     client.query("SELECT * FROM colis FULL OUTER JOIN users ON colis.email = users.email WHERE colis.isNotified=false",
      (err,res) =>{
         res.rows.forEach(function(colis){
-          if (colis.fb_id != null) facebook.callSendAPI(colis.fb_id,{"text":'Vous avez reçu un colis le ${colis.date}. Veuiller le collecter au foyer'})
+          if (colis.fb_id != null) facebook.callSendAPI(colis.fb_id,{"text":"Vous avez reçu un colis le ***. Veuiller le collecter au foyer"})
           client.query("UPDATE colis SET isnotified=true WHERE email= $1",[colis.email], () => null)
           //if not : send email notification ??
         })
