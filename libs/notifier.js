fb078c18394a154e3826e18b7b6950e5ac163b32
Youssef <dunnowuut@gmail.com>
2019-04-27 08:18:06 +0200

    notif. msg fixed

--- libs/notifier.js
+++ libs/notifier.js
@@ -14,9 +14,9 @@ module.exports = function(){
           let d = new Date(colis.date)
           let month = d.getMonth()+1
           let shortDate = d.getDate()+"-"+month+"-"+d.getFullYear()
-          if (colis.fb_id != null) facebook.callSendAPI(colis.fb_id,{"text":"Vous avez reçu un colis le "+shortDate+". Veuiller le collecter au foyer"})
+          let msg = "Vous venez de recevoir un colis. \n Date : "+shortDate+" \n Par : "+colis.sender+" \n Veuillez le collecter au foyer."
+          if (colis.fb_id != null) facebook.callSendAPI(colis.fb_id,{"text":msg})
           client.query("UPDATE colis SET isnotified=true WHERE email= $1",[colis.email], () => null)
-          //if not : send email notification ??
         })
     })
 }
