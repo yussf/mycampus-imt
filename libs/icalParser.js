0608cc9122f67a936823b585b2c19f99117943e1
Youssef <dunnowuut@gmail.com>
2019-05-17 06:23:23 +0200

    events order fixed in icalParser

--- libs/icalParser.js
+++ libs/icalParser.js
@@ -1,7 +1,7 @@
 const ical = require('ical');
 function formatTimeHHMMA(d) {
     function z(n){return (n<10?'0':'')+n}
-    var h = d.getHours() + 2
+    var h = d.getHours()
     return h + ':' + z(d.getMinutes())
   }
 function getDate(edtdate){
@@ -20,17 +20,19 @@ module.exports = (id, edtdate, callback) => {
     let text = ""
     const url = "http://edt.telecom-bretagne.eu/jsp/custom/modules/plannings/anonymous_cal.jsp?resources="+id+"&projectId=3&calType=ical&firstDate="+date+"&lastDate="+date
     ical.fromURL(url, {}, function (err, data) {
-        for (let k in data) {
-            if (data.hasOwnProperty(k)) {
-                var ev = data[k];
-                if (data[k].type == 'VEVENT') {
-                    text = text + `- ${ev.summary} | ${ev.location} | ${date} | ${formatTimeHHMMA(ev.start)} - ${formatTimeHHMMA(ev.end)}  \n \n`
-    
-                }
+        var listH = []
+        for (let k in data) listH.push([k, new Date(data[k].start).getTime()])
+        listH.sort((ele1,ele2) => {
+            return - (ele2[1] - ele1[1])
+        })
+        console.log(listH)
+        listH.forEach(e => {
+            var ev = data[e[0]]
+            if (ev.type == 'VEVENT') {
+                var subtext = `- ${ev.summary.trim()} | ${ev.location} | ${date} | ${formatTimeHHMMA(ev.start)} - ${formatTimeHHMMA(ev.end)}  \n \n`
+                text = text + subtext.replace("|  |", "|")
             }
-        }
+        })
         callback(text)
     })
 }
-
-
